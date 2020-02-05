5b44cf39ceb5be0856e6897a402a78ce2771c5fa
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2020-02-05 18:52:34 -0300

    Aula 15 - Objeto Math.

---
+++ aula15-Math/index.js
@@ -0,0 +1,16 @@
+let num1 = 9.54578;
+// let num2 = Math.floor(num1);    // Arredonda para baixo
+// let num2 = Math.ceil(num1);    // Arredonda para cima
+// let num2 = Math.round(num1);    // Arredonda para o mais próximo
+// console.log(num2);
+
+// console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,5));  // Retorna o maior número
+// console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,5));  // Retorna o menor número
+// console.log(Math.random()); // Retorna um número aleatório entre 0 e 1
+// const aleatorio = Math.round(Math.random() * (10 - 5) + 5); // Numero aleatorio entre 10 e 5
+// console.log(aleatorio);
+
+// console.log(Math.pow(2, 10));   // Potencia
+console.log(Math.sqrt(9));  // Raiz quadrada
+
+console.log(100/0); // Tipo de dado infinity

102274ec4274e9d7d6176937ec6eb91cdd69672e
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2020-04-04 18:05:37 -0300

    for e exercicio com for

---
+++ s2-logicaDeProgramacao/aula34-forClassico/index.js
@@ -0,0 +1,9 @@
+// for (let i = 0; i <= 5; i++) {
+//     console.log(`Linha ${i}`);
+// }
+
+const frutas = ['Maçã', 'Pêra', 'Uva'];
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(frutas[i]);
+}

6fda81dcf98b550b5dbadec6e71254fbbbd4313d
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2020-04-04 19:14:31 -0300

    For in e for of

---
+++ s2-logicaDeProgramacao/aula36-ForIn/index.js
@@ -0,0 +1,16 @@
+
+// const frutas = ['Pera', 'Maçã', 'Uva'];
+
+// for (let i in frutas) {
+//     console.log(frutas[i]);
+// }
+
+const pessoa = {
+    nome: 'Luis',
+    sobrenome: 'Otávio',
+    idade: 30
+};
+
+for (let chave in pessoa) {
+    console.log(chave, pessoa[chave]);
+}
---
+++ s2-logicaDeProgramacao/aula37-ForOf/index.js
@@ -0,0 +1,14 @@
+// Utilizado em objetos iteráveis
+
+const nome = 'Matheus Figueiredo';
+
+for (let valor of nome) {
+    console.log(valor);
+}
+
+// // Para arrays:
+// const nomes = ['Matheus', 'Alef', 'Daniel'];
+
+// nomes.forEach(function(valor, indice) {
+//     console.log(valor, indice);
+// });

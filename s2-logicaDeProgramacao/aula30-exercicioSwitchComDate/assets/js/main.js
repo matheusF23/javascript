33b6f330ef4d925c046aca18e1c5ac6f4d8843cd
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2020-04-04 16:57:24 -0300

    Exercicio com switch e date

---
+++ s2-logicaDeProgramacao/aula30-exercicioSwitchComDate/assets/js/main.js
@@ -0,0 +1,143 @@
+// const h1 = document.querySelector('.container h1');
+// const data = new Date();
+
+// function getDiaSemanaTexto(diaSemana) {
+//     let diaSemanaTexto;
+
+//     switch (diaSemana) {
+//         case 0:
+//             diaSemanaTexto = 'Domingo';
+//             return diaSemanaTexto;
+//         case 1:
+//             diaSemanaTexto = 'Segunda';
+//             return diaSemanaTexto;
+//         case 2:
+//             diaSemanaTexto = 'Terça';
+//             return diaSemanaTexto;
+//         case 3:
+//             diaSemanaTexto = 'Quarta';
+//             return diaSemanaTexto;
+//         case 4:
+//             diaSemanaTexto = 'Quinta';
+//             return diaSemanaTexto;
+//         case 5:
+//             diaSemanaTexto = 'Sexta';
+//             return diaSemanaTexto;
+//         case 6:
+//             diaSemanaTexto = 'Sábado';
+//             return diaSemanaTexto;
+//         default:
+//             diaSemanaTexto = '';
+//             return diaSemanaTexto;
+//     }
+// }
+
+// function getNomeMes(numeroMes) {
+//     let nomeMes;
+
+//     switch (numeroMes) {
+//         case 0:
+//             nomeMes = 'Janeiro';
+//             return nomeMes;
+//         case 1:
+//             nomeMes = 'Fevereiro';
+//             return nomeMes;
+//         case 2:
+//             nomeMes = 'Março';
+//             return nomeMes;
+//         case 3:
+//             nomeMes = 'Abril';
+//             return nomeMes;
+//         case 4:
+//             nomeMes = 'Maio';
+//             return nomeMes;;
+//         case 5:
+//             nomeMes = 'Junho';
+//             return nomeMes;
+//         case 6:
+//             nomeMes = 'Júlio';
+//             return nomeMes;
+//         case 7:
+//             nomeMes = 'Agosto';
+//             return nomeMes;
+//         case 8:
+//             nomeMes = 'Setembro';
+//             return nomeMes;
+//         case 9:
+//             nomeMes = 'Outubro';
+//             return nomeMes;
+//         case 10:
+//             nomeMes = 'Novembro';
+//             return nomeMes;
+//         case 11:
+//             nomeMes = 'Desembro';
+//             return nomeMes;
+//         default:
+//             nomeMes = '';
+//             return nomeMes;
+//     }
+// }
+
+// function zeroAEsquerda(num) {
+//     return num >= 10 ? num : `0${num}`
+// }
+
+// function criaData() {
+//     const diaSemana = data.getDay();
+//     const numeroMes = data.getMonth();
+
+//     const nomeDia = getDiaSemanaTexto(diaSemana);
+//     const nomeMes = getNomeMes(numeroMes);
+
+//     return (
+//         `${nomeDia}, ${data.getDate()} de ${nomeMes}` +
+//         `de ${data.getFullYear()},` +
+//         `${zeroAEsquerda(data.getHours())}:${zeroAEsquerda(data.getMinutes())}`
+//         );
+// }
+
+// h1.innerHTML = criaData(data);
+
+// Forma mais simples:
+
+// const h1 = document.querySelector('.container h1');
+// const data = new Date();
+
+// function getDiaSemanaTexto(diaSemana) {
+//     const diasSemana = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta',
+//         'Sexta', 'Sábado'];
+//     return diasSemana[diaSemana];
+// }
+
+// function getNomeMes(numeroMes) {
+//     const meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho',
+//         'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'Desembro'];
+//     return meses[numeroMes];
+// }
+
+// function zeroAEsquerda(num) {
+//     return num >= 10 ? num : `0${num}`
+// }
+
+// function criaData() {
+//     const diaSemana = data.getDay();
+//     const numeroMes = data.getMonth();
+
+//     const nomeDia = getDiaSemanaTexto(diaSemana);
+//     const nomeMes = getNomeMes(numeroMes);
+
+//     return (
+//         `${nomeDia}, ${data.getDate()} de ${nomeMes} ` +
+//         `de ${data.getFullYear()},` +
+//         `${zeroAEsquerda(data.getHours())}:${zeroAEsquerda(data.getMinutes())}`
+//     );
+// }
+
+// h1.innerHTML = criaData(data);
+
+// Forma PRO
+
+const h1 = document.querySelector('.container h1');
+const data = new Date();
+
+h1.innerHTML = data.toLocaleDateString('pt-BR', {dateStyle: 'full', timeStyle: 'short'});

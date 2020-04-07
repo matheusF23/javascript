f0e90e7b4a4757412e34f4138bc1c62da298a9f6
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2020-04-07 10:16:43 -0300

    retorno da funcao

---
+++ s3-funcoes/aula51-retornoDaFuncao/index.js
@@ -0,0 +1,10 @@
+function falaFrase(comeco) {
+    function falaResto(resto) {
+        return comeco + resto;
+    }
+    return falaResto;
+}
+
+const olaMundo = falaFrase('Olá');
+
+console.log(olaMundo(' mundo!'));

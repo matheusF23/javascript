27ff6ec1cce69928a60542fa8836fc926b4e54e6
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2020-04-07 11:24:23 -0300

    funcoes de callback

---
+++ s3-funcoes/aula54-funcoesDeCallback/index.js
@@ -0,0 +1,50 @@
+// funções de callback são executadas quando uma ação ocorre
+
+function rand(min = 1000, max = 3000) {
+  const num = Math.random() * (max - min) + min;
+  return Math.floor(num);
+}
+
+function f1(callback) {
+  setTimeout(function () {
+    console.log('f1');
+    if (callback) callback();
+  }, rand());
+}
+
+function f2(callback) {
+  setTimeout(function () {
+    console.log('f2');
+    if (callback) callback();
+  }, rand());
+}
+
+function f3(callback) {
+  setTimeout(function () {
+    console.log('f3');
+    if (callback) callback();
+  }, rand());
+}
+
+/* f1(function () {
+  f2(function () {
+    f3(function () {
+      console.log('Olá mundo');
+    });
+  });
+}); */
+
+// Para não criar a arvore de natal:
+f1(f1callback);
+
+function f1callback() {
+  f2(f2callback);
+}
+
+function f2callback() {
+  f3(f3callback);
+}
+
+function f3callback() {
+  console.log('Olá mundo');
+}

bd9903fedc08640a133c29345f6de146eaa85635
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2020-04-08 21:34:05 -0300

    revisão de arrays

---
+++ s4-array/aula62-revisao/index.js
@@ -0,0 +1,15 @@
+// Valor por referência
+
+// const nomes = ['Eduardo', 'Maria', 'joana'];
+// delete nomes[2]; // Remove um objeto do array sem remover o indice
+
+// const nomes = new Array('Eduardo', 'Maria', 'joana'); // rambem pode ser criado assim
+// console.log(nomes);
+
+// const novo = [...nomes];  // forma de copiar os dados do array nomes para um novo array
+                          // pq como o array pe um valor por referência fazendo apenas
+                          // novo = nomes, as duas variáveis estarão apontando para o mesmo array
+// console.log(novo);
+
+// const novo = nomes.slice(1, -1); // cria um novo array com um intervalo do array nomes
+// console.log(novo);

07653d04eabd096231b6a063db758a45feddee68
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2020-07-26 20:48:13 -0300

    atualizacao

--- s7-javascriptAssincrono/aula86-metodosUteisParaPromises.js
+++ s7-javascriptAssincrono/aula86-metodosUteisParaPromises.js
@@ -1,9 +1,3 @@
-function rand(min, max) {
-  min *= 1000;
-  max *= 1000;
-  return Math.floor(Math.random() * (max - min) + min);
-}
-
 function esperaAi(msg, tempo) {
   return new Promise((resolve, reject) => {
     if (typeof (msg) !== 'string') {
